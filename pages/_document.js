62c28739d9f339a9a0e697146f30552e14f8df5e
gagnerjeremy <gagner.jeremy@outlook.com>
2022-10-09 10:13:59 -0400

    Add reference to .png as favicon.

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/images/favicon.ico" />
+        <link rel="icon" href="/jginformatique-logo-officiel-icone.png" />
       </Head>
       <body>
         <Main />
