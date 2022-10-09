3a4798cdee83fa387882a8ac89f5056e5be22371
gagnerjeremy <gagner.jeremy@outlook.com>
2022-10-09 10:34:13 -0400

    Static folder for favicon

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-          <link rel="shortcut icon" href="/assets/favicon.ico" />
+          <link rel="shortcut icon" href="/static/favicon.ico" />
       </Head>
       <body>
         <Main />
